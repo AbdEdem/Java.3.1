2e0108d1794f42e986026a45ed2ea4b9760ef989
AbdEdem <87914622+AbdEdem@users.noreply.github.com>
2021-09-24 22:50:28 +0300

    Update rubl2.java

--- rubl2.java
+++ rubl2.java
@@ -3,7 +3,7 @@ public class rubl2 {
         int account = 1050;
         int balance = 100;
         int total = account + balance;
-        boolean ttt = account >= 1000;
+        boolean ttt = account > 1000;
 
         int bonus = (ttt ? account / 100 + total: account + balance);
         System.out.println(bonus);
